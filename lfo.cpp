04ee7050998adea5029f68aa497974d69a9982b8
mo-thunderz <76265418+mo-thunderz@users.noreply.github.com>
2020-12-27 23:02:59 +0100

    Add files via upload

--- lfo.cpp
+++ lfo.cpp
@@ -35,57 +35,56 @@ void lfo::setAmplOffset(int l_ampl_offset)
 	_ampl_offset = l_ampl_offset;
 }
 
-void lfo::setFreqSync(bool l_freq_sync)
+void lfo::setMode(bool l_mode)
 {
-	_freq_sync = l_freq_sync;
+	_mode = l_mode;
 }
 
-void lfo::setFreqMode0Free(float l_freq_mode0_free)
+void lfo::setMode0Freq(float l_mode0_freq)
 {
-	if(l_freq_mode0_free < 0)
-		l_freq_mode0_free = 0;
-	_freq_mode0_free = l_freq_mode0_free;
+	if(l_mode0_freq < 0)
+		l_mode0_freq = 0;
+	_mode0_freq = l_mode0_freq;
 }
 
-void lfo::setFreqMode0Free(float l_freq_mode0_free, unsigned long l_t)
+void lfo::setMode0Freq(float l_mode0_freq, unsigned long l_t)
 {
-	if(l_freq_mode0_free < 0)
-		l_freq_mode0_free = 0;
+	if(l_mode0_freq < 0)
+		l_mode0_freq = 0;
 
-	float l_phase_old = (double)(l_t - _t0) * _freq_mode0_free / 1000000 + _freq_mode0_phase;
-	float l_phase_new = (double)(l_t - _t0) * l_freq_mode0_free / 1000000;
+	// synchronize the phase of the old freq with the new freq
+	float l_phase_old = (double)(l_t - _t0) * _mode0_freq / 1000000 + _mode0_phase;
+	float l_phase_new = (double)(l_t - _t0) * l_mode0_freq / 1000000;
 	l_phase_old = l_phase_old - floor(l_phase_old);
 	l_phase_new = l_phase_new - floor(l_phase_new);
-	_freq_mode0_phase = l_phase_old - l_phase_new;
+	_mode0_phase = l_phase_old - l_phase_new;
 
-	_freq_mode0_free = l_freq_mode0_free;
+	_mode0_freq = l_mode0_freq;
 }
 
-void lfo::setFreqMode1Bpm(float l_freq_mode1_bpm)
+void lfo::setMode1Bpm(float l_mode1_bpm)
 {
-	if(l_freq_mode1_bpm < 0)
-		l_freq_mode1_bpm = 0;
-	_freq_mode1_bpm = l_freq_mode1_bpm;
+	if(l_mode1_bpm < 0)
+		l_mode1_bpm = 0;
+	_mode1_bpm = l_mode1_bpm;
 }
 
-void lfo::setFreqMode1Rate(int l_freq_mode1_rate)
+void lfo::setMode1Rate(float l_mode1_rate)
 {
-	if(l_freq_mode1_rate < 0)
-		l_freq_mode1_rate = 0;
-	if(l_freq_mode1_rate >= sizeof(_freq_array))
-		l_freq_mode1_rate = sizeof(_freq_array) - 1;
-	_freq_mode1_rate = l_freq_mode1_rate;
+	if(l_mode1_rate < 0)
+		l_mode1_rate = 0;
+	_mode1_rate = l_mode1_rate;
 }
 
-void lfo::setFreqMode1Phase(float l_freq_mode1_phase)
+void lfo::setMode1Phase(float l_mode1_phase)
 {
-	_freq_mode1_phase = l_freq_mode1_phase;
+	_mode1_phase = l_mode1_phase;
 }
 
 void lfo::sync(unsigned long l_t)
 {
 	_t0 = l_t;
-	_freq_mode0_phase = 0;
+	_mode0_phase = 0;
 }
 
 int lfo::getWaveForm()
@@ -103,9 +102,9 @@ int lfo::getAmplOffset()
 	return _ampl_offset;
 }
 
-float lfo::getFreqMode0Free()
+float lfo::getMode0Freq()
 {
-	return _freq_mode0_free;
+	return _mode0_freq;
 }
 
 int lfo::getWave(unsigned long l_t)
@@ -114,10 +113,10 @@ int lfo::getWave(unsigned long l_t)
 	int l_ampl = _ampl;
 	double l_phase = 0;
 
-	if(_freq_sync == false)         // LFO free running
-		l_phase = (double)(l_t - _t0) * _freq_mode0_free / 1000000 + _freq_mode0_phase;
+	if(_mode == false)         			// LFO free running
+		l_phase = (double)(l_t - _t0) * _mode0_freq / 1000000 + _mode0_phase;
 	else    												// LFO synced
-		l_phase = (double)(l_t - _t0) * _freq_array[_freq_mode1_rate] * _freq_mode1_bpm / 60000000 + _freq_mode1_phase;
+		l_phase = (double)(l_t - _t0) * _mode1_rate * _mode1_bpm / 60000000 + _mode1_phase;
 
 	// Compute correct _ampl_offsetoffset (wave not to exceed 0 to dacSize)
 	int l_ampl_offset = 0;
@@ -137,17 +136,17 @@ int lfo::getWave(unsigned long l_t)
 			return result = l_ampl * (_dacSize - 1 - ((int)(l_phase * _dacSize)) % _dacSize) / (_dacSize - 1) + l_ampl_offset - l_ampl_half;
 			break;
 		case 2: // Triangle
-			if(((int)(2 * l_phase))%2)
-				return result = abs(l_ampl * (_dacSize - ((int)(l_phase * _dacSize * 2)) % _dacSize - 1) / (_dacSize - 1) + l_ampl_offset - l_ampl_half);
-			else
+			if(((int)(2 * l_phase))%2) // up flank
 				return result = abs(l_ampl * (((int)(l_phase * _dacSize * 2)) % _dacSize) / (_dacSize - 1) + l_ampl_offset - l_ampl_half - 1);
+			else // down flank
+				return result = abs(l_ampl * (_dacSize - ((int)(l_phase * _dacSize * 2)) % _dacSize - 1) / (_dacSize - 1) + l_ampl_offset - l_ampl_half);
 			break;
 		case 3: // Sin
 			return result = (int) l_ampl_half * cos(2 * _PI * l_phase) + l_ampl_offset;
 			break;
 		case 4: // Square
 			if(((int)(2 * l_phase))%2)
-				return result = l_ampl_offset - l_ampl_half; 
+				return result = l_ampl_offset - l_ampl_half;
 			else
 				return result = l_ampl_offset + l_ampl_half;
 			break;
