242b499809dc3f991f7a04c6b95741fb57bfc755
mo-thunderz <76265418+mo-thunderz@users.noreply.github.com>
2020-12-29 23:23:31 +0100

    Add files via upload

--- lfo.h
+++ lfo.h
@@ -1,3 +1,10 @@
+//----------------------------------//
+// LFO class for Arduino
+// by mo-thunderz
+// version 1.1
+// last update: 29.12.2020
+//----------------------------------//
+
 #include "Arduino.h"
 
 #ifndef _PI
@@ -22,31 +29,37 @@ class lfo
 		void setMode0Freq(float l_mode0_freq, unsigned long l_t);							// set Freq in Hz of free-running mode, but by adding the current time the freq will be changed without phase jump -> just use micros() as second parameter
 	  void setMode1Bpm(float l_mode1_bpm);																	// set BPM of track for sync mode
 	  void setMode1Rate(float l_mode1_rate);																// set rate of sync mode -> see _freq_array -> if for instance l_mode1_rate is set to 15 this means the that _freq_array[15] = 1, so 1/1 will be the sync to the BPM.
-		void setMode1Phase(float l_mode1_phase);															// set phase offset for sync mode (free mode does not have phase offset as it is free running, though you can use sync(micros) in free mode to set phase to 0)
+		void setMode1Phase(float l_mode1_phase_offset);															// set phase offset for sync mode (free mode does not have phase offset as it is free running, though you can use sync(micros) in free mode to set phase to 0)
 		void sync(unsigned long l_t);																					// function to sync LFO to external trigger -> use sync(micros())
 
 		int getWaveForm();																										// simple get functions as variables are private
 		int getAmpl();
 		int getAmplOffset();
+		bool getMode();
 		float getMode0Freq();
+		float getMode1Rate();
+		float getPhase();																											// returns relative phase of output signal -> good for triggering LED
 		int getWave(unsigned long l_t);																				// main function that gives the waveformshape at time l_t -> use with getWave(micros())
 
 	private:
 		int 						_dacSize;														// DAC size
-		int 						_waveForm = 0;											// 0 -> off, 1 -> saw, 2 -> triangle, 3 -> sin, 4 -> square [0,4]
+		int 						_waveForm = 1;											// 0 -> off, 1 -> saw, 2 -> triangle, 3 -> sin, 4 -> square [0,4]
 		int 						_ampl = 0;													// amplitude, scales from 0 to _dacSize
 		int 						_ampl_offset = 0;										// amplitude offset, scales from 0 to _dacSize
 		bool            _mode = 0;        	      					// sync mode: false -> free, true -> synced to track
 		float           _mode0_freq = 30;           				// frequency in Hz
 		float           _mode1_bpm = 120;           				// BPM of track
 		float           _mode1_rate = 1;           					// Rate to link to BPM of track
-		float           _mode1_phase = 0;           				// phase of LFO -> scales from 0 to 1
+		float           _mode1_phase_offset = 0;           	// phase offset of LFO -> scales from 0 to 1
 
 		// time reference needed for sync waves:
 		unsigned long 	_t0 = 0;
 
 		// internal value to keep phase with freq changes in free mode:
-		float           _mode0_phase = 0;           		// phase of LFO in free-running mode -> this parameter is used internally to enable a seamless freq change with this function: setMode0Freq(float l_mode0_freq, unsigned long l_t);
+		float           _mode0_phase_offset = 0;           	// phase offset of LFO in free-running mode -> this parameter is used internally to enable a seamless freq change with this function: setMode0Freq(float l_mode0_freq, unsigned long l_t);
+
+		// phase of the signal -> is updated whenever getWave() is called -> this phase is useful for triggering an LED -> use getPhase() to retrieve.
+		float						_output_phase; 											// net phase of produced waveform -> used to trigger LEDs
 };
 
 #endif
